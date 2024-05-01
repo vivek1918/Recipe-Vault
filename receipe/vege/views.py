6b269b1dd2cef6336b9ac3c5fefc42cad5bbe4bb
vivekvasani <159635818+vivek1918@users.noreply.github.com>
2024-05-01 22:03:36 +0530

    Add files via upload

---
+++ receipe/vege/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.http import HttpResponse
+
+# Create your views here.
+def receipes(request):
+    if request.method == "POST":
+        data = request.POST
+        receipe_image = request.FILES.get('receipe_image')
+        receipe_name = data.get('receipe_name')
+        receipe_description = data.get('receipe_description')
+
+        Receipe.objects.create(
+            receipe_image=receipe_image,
+            receipe_description=receipe_description,
+            receipe_name=receipe_name
+        )
+        return redirect('/receipes/')
+    queryset = Receipe.objects.all()
+    context = {'receipes': queryset}
+    return render(request, 'receipes.html',context)
+
+def delete_receipe(request, id):
+    queryset = Receipe.objects.get(id=id)
+    queryset.delete()
+    return redirect('/receipes/')
+
+def update_receipe(request,id):
+    queryset = Receipe.objects.get(id=id) 
+
+    if request.method == "POST":
+        data = request.POST
+
+        receipe_image = request.FILES.get('receipe_image')
+        receipe_name = data.get('receipe_name')
+        receipe_description = data.get('receipe_description')
+        queryset.receipe_name = receipe_name
+        queryset.receipe_description = receipe_description
+
+        if receipe_image:
+            queryset.receipe_image=receipe_image
+        queryset.save()
+        return redirect('/receipes/')
+    context = {'receipe': queryset}
+    return render(request, 'update_receipes.html',context)
